724b7f458da2850d3782de2ce78a7bba904284f4
Irene-rgrc <82141991+Irene-rgrc@users.noreply.github.com>
2021-09-30 18:13:32 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+iimport java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args){
@@ -7,44 +7,47 @@ public class Menu {
         int opcion, num1, num2;
         
         do{
-            System.out.print("Introduzca un número");
+            System.out.print("Introduzca un número ");
             num1 = entrada.nextInt();
-            System.out.print("Introduzca otro un número");
+            System.out.print("Introduzca otro un número ");
             num2 = entrada.nextInt();
 
         do {
-            System.out.println("1, Suma");
-            System.out.println("2, Resta");
-            System.out.println("3, Multiplicación");
-            System.out.println("4, División");
-            System.out.println("5, Resto");
-            System.out.println("6, Salir");
+            System.out.println(" ");
+            System.out.println("1, Suma ");
+            System.out.println("2, Resta ");
+            System.out.println("3, Multiplicación ");
+            System.out.println("4, División ");
+            System.out.println("5, Resto ");
+            System.out.println("6, Salir ");
+            System.out.println(" ");
             
             opcion = entrada.nextInt(); 
             
             switch (opcion) {
                 case 1:
-                    System.out.print("La suma es" + (num1+num2));
+                    System.out.print("La suma es " + (num1+num2));
                     break;
                 case 2:
-                    System.out.print("La resta es"+ (num1-num2));
+                    System.out.println("La resta es " + (num1-num2));
                     break;
                 case 3:
-                    System.out.print("La multiplicacion es"+ (num1 * num2));
+                    System.out.print("La multiplicacion es "+ (num1 * num2));
                     break;   
                 case 4:
-                    System.out.print("La division es"+ (num1/num2));
+                    System.out.print("La division es "+ (num1/num2));
                     break;
                 case 5:
-                    System.out.print("La resto es"+ (num1%num2));
+                    System.out.print("La resto es "+ (num1%num2));
                     break;
                 case 6:
-                    System.out.print("Adios");
+                    System.out.println("Adios");
                     break;
                 
                 default:
                     System.out.print("Opcion no correcta");      
             }
+            
  
         } while (opcion != 6);
         
