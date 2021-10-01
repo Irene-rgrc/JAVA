9f2f66feeff8bd0ece473ae46977c894c28f03d0
Irene <ireneneri001@gmail.com>
2021-10-01 11:37:38 +0200

    Update Tarea2.java

--- Tema 2/Tarea2.java
+++ Tema 2/Tarea2.java
@@ -9,51 +9,45 @@ public class Tarea2 {
         String cadena1;
         String cadena2;
         double num1, num2, angulo;
-        //*COMPARACIÓN DE DOS CADENAS
-        do {
-            System.out.println("-----------CADENAS-----------");
-            System.out.println("Introduzca la primera cadena : ");
-            cadena1 = entrada.nextLine();
-            System.out.println("Introduzca la segunda cadena : ");
-            cadena2 = entrada.nextLine();
+        System.out.println("-----------CADENAS-----------");
+        System.out.println("Introduzca la primera cadena : ");
+        cadena1 = entrada.nextLine();
+        System.out.println("Introduzca la segunda cadena : ");
+        cadena2 = entrada.nextLine();
             
-            System.out.println("La longuitud de " + cadena1 + " es "+ cadena1.length());
-            System.out.println("La longuitud de " + cadena2 + " es "+ cadena2.length());
+        System.out.println("La longuitud de " + cadena1 + " es "+ cadena1.length());
+        System.out.println("La longuitud de " + cadena2 + " es "+ cadena2.length());
             
-            if(cadena1.compareTo(cadena2)==0){
-                System.out.println("¿Son iguales las cadenas? True");
-            }
-            else{
-                System.out.println("¿Son iguales las cadenas? False");
-            }
+        if(cadena1.compareTo(cadena2)==0){
+            System.out.println("¿Son iguales las cadenas? True");
+          }
+        else{
+            System.out.println("¿Son iguales las cadenas? False");
+          }
             
-            System.out.println("¿Son iguales las cadenas ignorando las minusculas y mayusculas?"+ cadena1.equalsIgnoreCase(cadena2));
+        System.out.println("¿Son iguales las cadenas ignorando las minusculas y mayusculas?"+ cadena1.equalsIgnoreCase(cadena2));
             
-            System.out.println("Primera cadena en mayusculas "+ cadena1.toUpperCase());
-            System.out.println("Segunda cadena en mayusculas "+ cadena2.toLowerCase());
+        System.out.println("Primera cadena en mayusculas "+ cadena1.toUpperCase());
+        System.out.println("Segunda cadena en mayusculas "+ cadena2.toLowerCase());
             
-            if (cadena1.contains("a")){
-                System.out.print("Indice de la primera aparición de 'a' : "+ cadena1.indexOf('a'));
-                System.out.print("Indice de la última aparición de 'a' : "+ cadena1.lastIndexOf('a'));
-            }
+        if (cadena1.contains("a")){
+            System.out.print("Indice de la primera aparición de 'a' : "+ cadena1.indexOf('a'));
+            System.out.print("Indice de la última aparición de 'a' : "+ cadena1.lastIndexOf('a'));
+          }
                     
             
             
     
         //*TRIANGULO
         
-            System.out.println("----------TRIANGULO----------");
-            System.out.println("Introduzca el primer lado : ");
-            num1 = entrada.nextInt();
-            System.out.println("Introduzca el otro lado : ");
-            num2 = entrada.nextInt();
-            System.out.println("Introduzca el angulo : ");
-            angulo = entrada.nextInt();
+        System.out.println("----------TRIANGULO----------");
+        System.out.println("Introduzca el primer lado : ");
+        num1 = entrada.nextInt();
+        System.out.println("Introduzca el otro lado : ");
+        num2 = entrada.nextInt();
+        System.out.println("Introduzca el angulo : ");
+        angulo = entrada.nextInt();
             
-            System.out.print("El área del triangulo es : "+ ((num1*num2*sin(angulo))/2))
-            System.out.println(" ");
+        System.out.print("El área del triangulo es : "+ ((num1*num2*Math.sin((angulo))/2)));
+        System.out.println(" ");
 
-        
-    }while (True);
-  }
-} 
