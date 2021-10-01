e6328072e91b112e0e74c6bd7b7742261ee50a94
Irene <ireneneri001@gmail.com>
2021-10-01 10:38:28 +0200

    Create Tarea2.java

---
+++ Tema 2/Tarea2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Tarea2 {
+    public static void main(String[] args){
+        
+        Scanner entrada = new Scanner(System.in);
+        int num1, num2;
+        //*COMPARACIÓN DE DOS CADENAS
+        
+        //*TRIANGULO
+        do{
+            System.out.println("----------TRIANGULO----------");
+            System.out.print("Introduzca el primer lado : ");
+            num1 = entrada.nextInt();
+            System.out.print("Introduzca el otro lado : ");
+            num2 = entrada.nextInt();
+            System.out.print("Introduzca el angulo : ");
+            num2 = entrada.nextInt();
+            
+            System.out.print("El área del triangulo es : "+ (num1*num2))
+            System.out.println(" ");
+
+        
+        
+    }while (True); //MODIFICAR DESPUES
+  }
+}
+    
