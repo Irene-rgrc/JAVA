11fd70399e69f202843cdcede8edb5909ab5e993
Irene <ireneneri001@gmail.com>
2021-10-26 17:24:24 +0200

    Update Menu.java

--- Tema 2/Menu.java
+++ Tema 2/Menu.java
@@ -1,37 +1,58 @@
 import java.util.Scanner;
 
-public class Tarea2 {
+public class Menu {
     public static void main(String[] args){
-        System.out.println("Programa realizado por : ");
-        System.out.println("Irene Rodriguez García");
-        System.out.println("--------------------------------");
+        
         Scanner entrada = new Scanner(System.in);
-        String 
-        int num1, num2, angulo;
-        //*COMPARACIÓN DE DOS CADENAS
-        do {
-            System.out.println("-----------CADENAS-----------");
-            System.out.print("Introduzca el primer lado : ");
+        int opcion, num1, num2;
         
-    
-        //*TRIANGULO
-        
-            System.out.println("----------TRIANGULO----------");
-            System.out.print("Introduzca el primer lado : ");
+        do{
+            System.out.print("Introduzca un número ");
             num1 = entrada.nextInt();
-            System.out.print("Introduzca el otro lado : ");
+            System.out.print("Introduzca otro un número ");
             num2 = entrada.nextInt();
-            System.out.print("Introduzca el angulo : ");
-            angulo = entrada.nextInt();
+
+        do {
+            System.out.println(" ");
+            System.out.println("1, Suma ");
+            System.out.println("2, Resta ");
+            System.out.println("3, Multiplicación ");
+            System.out.println("4, División ");
+            System.out.println("5, Resto ");
+            System.out.println("6, Salir ");
             System.out.println(" ");
             
-            System.out.print("El área del triangulo es : "+ ((num1*num2*sin(angulo))/2))
-            System.out.println(" ");
-
+            opcion = entrada.nextInt(); 
+            
+            switch (opcion) {
+                case 1:
+                    System.out.print("La suma es " + (num1+num2));
+                    break;
+                case 2:
+                    System.out.println("La resta es " + (num1-num2));
+                    break;
+                case 3:
+                    System.out.print("La multiplicacion es "+ (num1 * num2));
+                    break;   
+                case 4:
+                    System.out.print("La division es "+ (num1/num2));
+                    break;
+                case 5:
+                    System.out.print("La resto es "+ (num1%num2));
+                    break;
+                case 6:
+                    System.out.println("Adios");
+                    break;
+                
+                default:
+                    System.out.print("Opcion no correcta");      
+            }
+            
+ 
+        } while (opcion != 6);
         
-        
-        
-        
-    }while (True);
+    }while (num1 > 0 && num2 > 0);
   }
-} 
+}
+    
+
