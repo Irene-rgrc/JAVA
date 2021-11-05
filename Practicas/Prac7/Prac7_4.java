ff7d4fa6c298936f0272655d6db9c1e864b19724
Irene <ireneneri001@gmail.com>
2021-11-05 11:12:42 +0100

    Update Prac7_4.java

--- Practicas/Prac7/Prac7_4.java
+++ Practicas/Prac7/Prac7_4.java
@@ -1,20 +1,19 @@
-public class Prac7_4 {
-    
-    public static int pot (int b, int e) {
-        if (e>0){
-            return b + pot(b,e-1);
-        }//fin si
-        return 1;  
+public class Prac7_3 {
+
+    public static int pot(int b, int e) {
+        if (e == 0) {
+            return 1;
+        } else {
+            return b * pot(b, e - 1);
+        }
     }
-    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int potencia = pot(2,4);
+        int potencia = pot(2, 4);
         System.out.println("La potencia es " + potencia);
     }
-    
 }
